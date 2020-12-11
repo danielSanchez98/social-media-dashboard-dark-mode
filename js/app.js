8c9747532a389803f6f0aabd35258537a7f5f4d1
Daniel <ds980220@gmail.com>
2020-12-10 19:11:06 -0500

    refactor css variables

--- js/app.js
+++ js/app.js
@@ -1,5 +1,10 @@
 const checkbox = document.querySelector('#checkbox');
 
-if(window.matchMedia('(prefers-color-scheme: dark)').matches){
+
+if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
     checkbox.setAttribute('checked', true);
 }
+
+checkbox.addEventListener('change', function (evt) {
+    document.body.classList.toggle('is-dark-mode')
+})
