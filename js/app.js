f9c37f26c155342a77299b1106a8a857ea34ee1b
Daniel <ds980220@gmail.com>
2020-12-10 19:38:58 -0500

    tswitch to toggle dark mode

--- js/app.js
+++ js/app.js
@@ -1,10 +1,17 @@
 const checkbox = document.querySelector('#checkbox');
+const mediaQueryDark = window.matchMedia('(prefers-color-scheme: dark)');
 
-
-if (window.matchMedia('(prefers-color-scheme: dark)').matches) {
+if (mediaQueryDark.matches) {
     checkbox.setAttribute('checked', true);
 }
-
-checkbox.addEventListener('change', function (evt) {
-    document.body.classList.toggle('is-dark-mode')
-})
+function toggleDarkMode(evt) {
+    if (this.checked) {
+        document.body.classList.remove('is-light-mode');
+        document.body.classList.add('is-dark-mode');
+    } else {
+        document.body.classList.remove('is-dark-mode');
+        document.body.classList.add('is-light-mode');
+    }
+}
+mediaQueryDark.addEventListener('change', toggleDarkMode)
+checkbox.addEventListener('change', toggleDarkMode)
